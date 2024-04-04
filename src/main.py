fae0686022e1a10d5310786b581c8f4a85a32703
cmwt <163149425+cmwt@users.noreply.github.com>
2024-04-04 19:18:17 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,3 @@
+# The purpose of this file is to build the GUI application with the tkinter module,
+# and to call on other files within the 'data' directory to use the functionalities of the
+# application. 
