df1b4a52ac2eb22f81f34980aac06990a7fa9931
cmwt <163149425+cmwt@users.noreply.github.com>
2024-04-04 21:40:00 +0200

    Update main.py
    Adds a space at the top of the window where tools can be inserted.

--- src/main.py
+++ src/main.py
@@ -6,12 +6,10 @@ import tkinter as tk # Import the tkinter module in order to build the GUI
 from tkinter import *
 
 window = tk.Tk() # Initiate a window
-
-w = tk.Label (window, text="Hello, CW4")
-w.pack()
+toolbar = tk.Frame(window, background="light grey", height = 30) # Defines a toolbar at the top of the window
+toolbar.pack(side="top", fill="x") # Inserts the toolbar into the window
 
 # Sets different visual attributes about the window, such as title, size, etc.
 window.title("CW4 Dr. Jolley Project")
-window.geometry("400x350+500+500")
 
 window.mainloop()
