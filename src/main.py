2cd58ab339257db459d1e8dc8eadede500a3677a
Will Thomas <163149425+cmwt@users.noreply.github.com>
2024-04-22 22:01:05 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,31 +2,33 @@
 # and to call on other files within the 'data' directory to use the functionalities of the
 # application. 
 
-import tkinter as tk # Import the tkinter module in order to build the GUI
-from tkinter import *
-
-window = tk.Tk() # Initiate a window
-
-def show():
-    label.config( text = Element.get() )
-
-options = [
+elements = [
     "Carbon",
     "Hydrogen",
     "Oxygen",
     "Sulfur"
 ]
 
+import tkinter as tk # Import the tkinter module in order to build the GUI
+from tkinter import *
+
+root = tk.Tk() # Initiate a window
+root.geometry("1500x750")
+
+frame = tk.Frame(root, width = 300, height = 750, borderwidth = 5, relief = tk.GROOVE)
+frame.pack_propagate(False)
+frame.pack(side="left")
+
+frame1 = tk.Frame(root, width = 1200, height = 750, borderwidth = 5, relief = tk.GROOVE)
+frame1.pack_propagate(False)
+frame1.pack(side="right")
+
 element = StringVar()
-element.set("Carbon")
-drop = OptionMenu(window, element, *options)
-drop.pack(side = "top")
-
-# The following code formats the file menu at the top of the GUI window
-menubar = Menu(window)
-
+element.set("Element")
+drop = OptionMenu(frame, element, *elements)
+drop.pack()
+         
 # Sets different visual attributes about the window, such as title, size, etc.
-window.title("CW4 Project - Molecular Dynamics")
-window.attributes('-fullscreen', True)
+root.title("CW4 Project - Molecular Dynamics")
 
-window.mainloop()
+root.mainloop()
