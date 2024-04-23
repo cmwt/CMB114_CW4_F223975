0311144744ef2a07d11037bf8baf06b2eee83a83
Will Thomas <163149425+cmwt@users.noreply.github.com>
2024-04-23 17:43:48 +0100

    Update main.py
    Added a toolbar at the top of the window and tidied up the code with some comments to organise it.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@
 # and to call on other files within the 'data' directory to use the functionalities of the
 # application. 
 
-elements = [
-    "Carbon",
-    "Hydrogen",
-    "Oxygen",
-    "Sulfur"
+molecules = [
+    "Benzene",
+    "Naphthalene",
+    "Cyclohexane",
+    "Acetone"
 ]
 
 import tkinter as tk # Import the tkinter module in order to build the GUI
@@ -15,19 +15,32 @@ from tkinter import *
 root = tk.Tk() # Initiate a window
 root.geometry("1500x750")
 
+# ****** Toolbar ******
+
+toolbar = tk.Frame(root, borderwidth = 5, relief = tk.GROOVE)
+toolbar.pack(side="top", fill = "x")
+select_but = tk.Button(toolbar, text="Select")
+select_but.pack(side="left", padx = 2, pady = 2)
+nav_but = tk.Button(toolbar, text = "Navigate")
+nav_but.pack(side = "left", padx = 2, pady = 2)
+
+# ****** Left Pane ******
+
 frame = tk.Frame(root, width = 300, height = 750, borderwidth = 5, relief = tk.GROOVE)
 frame.pack_propagate(False)
-frame.pack(side="left")
+frame.pack(side="left", fill = "y")
+
+molecule = StringVar()
+molecule.set("Molecule")
+drop = OptionMenu(frame, molecule, *molecules)
+drop.place(x = 0, y = 0)
+
+# ****** Right Frame ******
 
 frame1 = tk.Frame(root, width = 1200, height = 750, borderwidth = 5, relief = tk.GROOVE)
 frame1.pack_propagate(False)
-frame1.pack(side="right")
+frame1.pack(side="right", fill = "y")
 
-element = StringVar()
-element.set("Element")
-drop = OptionMenu(frame, element, *elements)
-drop.pack()
-         
 # Sets different visual attributes about the window, such as title, size, etc.
 root.title("CW4 Project - Molecular Dynamics")
 
