9d058d190f5e72d96326ad01ef5133045f63277f
Will Thomas <163149425+cmwt@users.noreply.github.com>
2024-05-08 16:52:45 +0100

    Update main.py
    Added functionality for conditional drawing of molecules dependant on which option is selected from the combobox

--- src/main.py
+++ src/main.py
@@ -2,7 +2,48 @@
 # and to call on other files within the 'data' directory to use the functionalities of the
 # application. 
 
+# *** List and functions regarding molecules ***
+
 molecules = ["Benzene", "Naphthalene", "Cyclohexane", "Acetone"]
+
+#reference_text = {"Benzene":"Molar Mass: 78.11 g/mol",
+                  #"Naphthalene":"Molar Mass: 128.17 g/mol",
+                  #}
+
+def draw_benz():
+    benz_canvas = tk.Canvas(frame1)
+    benz_canvas.create_image(0, 0, anchor=tk.NW, image=benz_photo)
+    benz_canvas.grid(row=1, column=1)
+
+def draw_naph():
+    naph_canvas = tk.Canvas(frame1)
+    naph_canvas.create_image(0, 0, anchor=tk.NW, image=naph_photo)
+    naph_canvas.grid(row=1, column=2)
+
+def draw_cyhex():
+    cyhex_canvas = tk.Canvas(frame1)
+    cyhex_canvas.create_image(0, 0, anchor=tk.NW, image=cyhex_photo)
+    cyhex_canvas.grid(row=1, column=3)
+
+def draw_ace():
+ ace_canvas = tk.Canvas(frame1)
+ ace_canvas.create_image(0, 0, anchor=tk.NW, image=ace_photo)
+ ace_canvas.grid(row=2, column=1)
+
+def option_selected(event):
+    if drop.get() == "Benzene":
+        draw_benz()
+    if drop.get() == "Naphthalene":
+        draw_naph()
+    if drop.get() == "Cyclohexane":
+        draw_cyhex()
+    if drop.get() == "Acetone":
+        draw_ace()
+
+# Unfinished text function
+#def reference_text():
+    #if drop.get() == "Benzene":
+        #reference_label.config(text = reference_text("Benzene"))
 
 # Defining a function to turn atom number button on/off
 
@@ -18,6 +59,11 @@ def atom_switch():
         atom_num.config(text = "On") # Switches the button to "On" when clicked again
         atom_on = True
 
+# Clear function erase contents of right frame
+
+def clear_frame():
+    for widgets in frame1.winfo_children():
+        widgets.destroy()
 
 import tkinter as tk # Import the tkinter module in order to build the GUI
 from tkinter import ttk
@@ -26,7 +72,7 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 
 root = tk.Tk() # Initiate a window
-root.geometry("1500x750")
+root.geometry("570x750")
 root.configure( background = "white")
 
 # ***** Molecules *****
@@ -57,26 +103,30 @@ ace_photo = ImageTk.PhotoImage(ace_img)
 
 # ****** Toolbar ******
 
-toolbar = ttk.Frame(root, borderwidth = 5,  relief = tk.GROOVE)
-toolbar.pack(side="top", fill = "x")
-select_but = ttk.Button(toolbar, text="Select")
-select_but.pack(side="left", padx = 2, pady = 2)
-nav_but = ttk.Button(toolbar, text = "Navigate")
-nav_but.pack(side = "left", padx = 2, pady = 2)
+#toolbar = ttk.Frame(root, borderwidth = 5,  relief = tk.GROOVE)
+#toolbar.pack(side="top", fill = "x")
+#select_but = ttk.Button(toolbar, text="Select")
+#select_but.pack(side="left", padx = 2, pady = 2)
+#nav_but = ttk.Button(toolbar, text = "Navigate")
+#nav_but.pack(side = "left", padx = 2, pady = 2)
 
-# ****** Left Pane ******
+# ****** Left Frame ******
 
 frame = ttk.Frame(root, width = 300, height = 750, borderwidth = 5, relief = tk.GROOVE)
 frame.pack_propagate(False)
 frame.pack(side="left", fill = "y")
 
 drop = ttk.Combobox(frame, value= molecules)
-drop.pack(pady = 5)
+drop.grid(row=1, column=1, pady = 5)
+drop.bind("<<ComboboxSelected>>", option_selected)
+
+clear_but = tk.Button(frame, text="Clear", command=clear_frame)
+clear_but.grid(row=1, column=2, pady=5, padx=5)
 
 atom_num = tk.Button(frame, text = "On", command=atom_switch)
 atom_label = tk.Label(frame, text= "Atom Numbers:")
-atom_label.pack(pady=2)
-atom_num.pack(pady = 2)
+atom_label.grid(row=2, column=1, pady=2)
+atom_num.grid(row=3, column=1, pady = 2)
 
 # ****** Right Frame ******
 
@@ -84,23 +134,9 @@ frame1 = ttk.Frame(root, width = 1200, height = 750, borderwidth = 5, relief = t
 frame1.pack_propagate(False)
 frame1.pack(side="left", fill = "both")
 
-benz_canvas = tk.Canvas(frame1)
-benz_canvas.create_image(0, 0, anchor=tk.NW, image=benz_photo)
-benz_canvas.grid(row=1, column=1)
-
-naph_canvas = tk.Canvas(frame1)
-naph_canvas.create_image(0, 0, anchor=tk.NW, image=naph_photo)
-naph_canvas.grid(row=1, column=2)
-
-cyhex_canvas = tk.Canvas(frame1)
-cyhex_canvas.create_image(0, 0, anchor=tk.NW, image=cyhex_photo)
-cyhex_canvas.grid(row=1, column=3)
-
-ace_canvas = tk.Canvas(frame1)
-ace_canvas.create_image(0, 0, anchor=tk.NW, image=ace_photo)
-ace_canvas.grid(row=2, column=1)
+#reference_label = tk.Label(frame1)
+#reference_label.grid(row=4, column=1)
 
 root.title("CW4 Project - Molecular Dynamics")
-frame1.config(row=3, column=3)
 
 root.mainloop()
