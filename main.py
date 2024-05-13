bf719a39e1e3dc2c3e1b6fb19c5854500ef445df
Will Thomas <163149425+cmwt@users.noreply.github.com>
2024-05-13 20:43:57 +0100

    Update main.py
    moved some things to a separate .py file in another directory so that a vast library of molecules can be written into the program later.
    added reference text function but needs refining

--- main.py
+++ main.py
@@ -2,13 +2,32 @@
 # and to call on other files within the 'data' directory to use the functionalities of the
 # application. 
 
-# *** List and functions regarding molecules ***
+# Unfinished text function, ask Jolley
+def reference_text(x):
+    reference_label.config(text = text.get(x))
 
-molecules = ["Benzene", "Naphthalene", "Cyclohexane", "Acetone"]
+# Defining a function to turn atom number button on/off
 
-#reference_text = {"Benzene":"Molar Mass: 78.11 g/mol",
-                  #"Naphthalene":"Molar Mass: 128.17 g/mol",
-                  #}
+global atom_on # Sets button to "On" automatically when the GUI starts
+atom_on = True
+
+def atom_switch():
+    global atom_on
+    if atom_on:
+       atom_num.config(text = "Off") # Switches the button to "Off" when clicked
+       atom_on = False 
+    else:
+        atom_num.config(text = "On") # Switches the button to "On" when clicked again
+        atom_on = True
+def option_selected(event):
+    if drop.get() == "Benzene":
+        draw_benz()
+    if drop.get() == "Naphthalene":
+        draw_naph()
+    if drop.get() == "Cyclohexane":
+        draw_cyhex()
+    if drop.get() == "Acetone":
+        draw_ace()
 
 def draw_benz():
     benz_canvas = tk.Canvas(frame1)
@@ -30,35 +49,6 @@ def draw_ace():
  ace_canvas.create_image(0, 0, anchor=tk.NW, image=ace_photo)
  ace_canvas.grid(row=2, column=1)
 
-def option_selected(event):
-    if drop.get() == "Benzene":
-        draw_benz()
-    if drop.get() == "Naphthalene":
-        draw_naph()
-    if drop.get() == "Cyclohexane":
-        draw_cyhex()
-    if drop.get() == "Acetone":
-        draw_ace()
-
-# Unfinished text function
-#def reference_text():
-    #if drop.get() == "Benzene":
-        #reference_label.config(text = reference_text("Benzene"))
-
-# Defining a function to turn atom number button on/off
-
-global atom_on # Sets button to "On" automatically when the GUI starts
-atom_on = True
-
-def atom_switch():
-    global atom_on
-    if atom_on:
-       atom_num.config(text = "Off") # Switches the button to "Off" when clicked
-       atom_on = False 
-    else:
-        atom_num.config(text = "On") # Switches the button to "On" when clicked again
-        atom_on = True
-
 # Clear function erase contents of right frame
 
 def clear_frame():
@@ -70,51 +60,23 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from rdkit import Chem
 from rdkit.Chem import Draw
+from lib.mol import *
 
 root = tk.Tk() # Initiate a window
 root.geometry("570x750")
 root.configure( background = "white")
+root.title("CW4 Project - Molecular Dynamics")
 
-# ***** Molecules *****
+ref_var = tk.StringVar()
+ref_var.set("")
 
-# Benzene
-benzene_SMILES = "c1ccccc1" # Assigns benzene a SIMLES moleucle format
-benzene = Chem.MolFromSmiles(benzene_SMILES) # Creates the SMILES molecule
-benz_img = Draw.MolToImage(benzene) # Creates the benzene image
-benz_photo = ImageTk.PhotoImage(benz_img) # Puts the benzene image onto a tkinter PhotoImage
+# *** Left Frame ***
 
-# Naphthalene
-naph_SMILES = "c1c2ccccc2ccc1"
-naph = Chem.MolFromSmiles(naph_SMILES) 
-naph_img = Draw.MolToImage(naph) 
-naph_photo = ImageTk.PhotoImage(naph_img)
-
-# Cyclohexane
-cyhex_SMILES = "C1CCCCC1"
-cyhex = Chem.MolFromSmiles(cyhex_SMILES) 
-cyhex_img = Draw.MolToImage(cyhex) 
-cyhex_photo = ImageTk.PhotoImage(cyhex_img)
-
-# Acetone
-ace_SMILES = "CC(=O)C"
-ace = Chem.MolFromSmiles(ace_SMILES) 
-ace_img = Draw.MolToImage(ace) 
-ace_photo = ImageTk.PhotoImage(ace_img)
-
-# ****** Toolbar ******
-
-#toolbar = ttk.Frame(root, borderwidth = 5,  relief = tk.GROOVE)
-#toolbar.pack(side="top", fill = "x")
-#select_but = ttk.Button(toolbar, text="Select")
-#select_but.pack(side="left", padx = 2, pady = 2)
-#nav_but = ttk.Button(toolbar, text = "Navigate")
-#nav_but.pack(side = "left", padx = 2, pady = 2)
-
-# ****** Left Frame ******
+molecules = ["Benzene", "Naphthalene", "Cyclohexane", "Acetone"]
 
 frame = ttk.Frame(root, width = 300, height = 750, borderwidth = 5, relief = tk.GROOVE)
 frame.pack_propagate(False)
-frame.pack(side="left", fill = "y")
+frame.pack(side="left", fill = "both", expand=1)
 
 drop = ttk.Combobox(frame, value= molecules)
 drop.grid(row=1, column=1, pady = 5)
@@ -128,15 +90,31 @@ atom_label = tk.Label(frame, text= "Atom Numbers:")
 atom_label.grid(row=2, column=1, pady=2)
 atom_num.grid(row=3, column=1, pady = 2)
 
-# ****** Right Frame ******
+reference_label = tk.Label(frame) # Ask Jolley
+reference_label.grid(row=4, column=1)
+
+text = {"Benzene":"Molar Mass: 78.11 g/mol\nBoiling Point: 80.1°C\nMelting Point: 5.5°C",
+        "Naphthalene":"Molar Mass: 128.17 g/mol",
+                  }
+
+# *** Right Frame ***
 
 frame1 = ttk.Frame(root, width = 1200, height = 750, borderwidth = 5, relief = tk.GROOVE)
 frame1.pack_propagate(False)
-frame1.pack(side="left", fill = "both")
+frame1.pack(side="left", fill = "both", expand=1)
 
-#reference_label = tk.Label(frame1)
-#reference_label.grid(row=4, column=1)
+# *** Molecule images ***
 
-root.title("CW4 Project - Molecular Dynamics")
+benz_img = Draw.MolToImage(benzene) # Creates the benzene image
+benz_photo = ImageTk.PhotoImage(benz_img) # Puts the benzene image onto a tkinter PhotoImage
+
+naph_img = Draw.MolToImage(naph) 
+naph_photo = ImageTk.PhotoImage(naph_img)
+
+cyhex_img = Draw.MolToImage(cyhex) 
+cyhex_photo = ImageTk.PhotoImage(cyhex_img)
+
+ace_img = Draw.MolToImage(ace) 
+ace_photo = ImageTk.PhotoImage(ace_img)
 
 root.mainloop()
