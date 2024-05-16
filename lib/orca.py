58d773a3085209d94bdbaadfbcfa908272be4f40
Will Thomas <163149425+cmwt@users.noreply.github.com>
2024-05-16 22:46:59 +0100

    Create orca.py
    sets lists from which the ORCA file can be formatted

---
+++ lib/orca.py
@@ -0,0 +1,6 @@
+# This file contains the necessary lists for the ORCA functionality of
+# the application.
+
+basis_sets = ["def2-SVP", "def2-TZVP", "6-31G", "cc-pVDZ"]
+
+calc_types = ["HF", "DFT", "MP2", "CCSD"]
